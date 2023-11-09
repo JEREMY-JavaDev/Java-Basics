779e7507b9a949db75cc75049a30c2ca55001726
Jeremy Java/Angular <148239149+JEREMY-JavaDev@users.noreply.github.com>
2023-11-09 16:38:09 +0100

    Add files via upload

---
+++ Java 01 Afficharge/src/Principale.java
@@ -0,0 +1,13 @@
+/* Ce programme s'execute dans la console d'éclipse.
+ * *programme: affiche Hello world à l'écran
+ * Auteur: JEREMY-JavaDev
+ */
+
+public class Principale { // ne pas modifier cette ligne
+
+	public static void main(String[] args) {
+		// cette méthode est lepoint d'entrée du programme
+		System.out.println("Hello world");
+	}
+
+}
